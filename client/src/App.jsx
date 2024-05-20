c16e82fe8e9287bac305d315ef43289ca558f0ce
Sanjay Vinod Jacob <sanjayvinodjacob@gmail.com>
2024-05-20 20:08:59 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import io from "socket.io-client";
 import Chat from "./Chat"
 
-const socket = io.connect("https://connect-react-server.vercel.app/");
+const socket = io.connect("https://connect-react-server.vercel.app");
 
 function App() {
   const [username, setUsername] = useState("")
