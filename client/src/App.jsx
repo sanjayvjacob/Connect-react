17c08dc7943aba40ae8f0e6b6ce5b6a34720a6ac
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-07-11 00:44:20 +0100

    fixed project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import io from "socket.io-client";
 import Chat from "./Chat"
 
-const socket = io.connect("https://connect-react-client.vercel.app/");
+const socket = io.connect("http://localhost:5174");
 
 function App() {
   const [username, setUsername] = useState("")
--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://connect-react-server.vercel.app/",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
@@ -28,10 +28,6 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
   });
-});
-
-app.get("/", (req, res) => {
-  res.send("Server is running");
 });
 
 server.listen(5174, () => {
